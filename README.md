7121754f07f09eecce1f45ff6d2c2f6cbf4df45f
raphael <raphael.birkl@gmail.com>
2024-01-23 11:29:17 +0100

    readme & added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tasksheet Input Validation
+
+![logo_app_akademie](assets/app_akademie_logo.png)
+
+## A tasksheet about Input Validation
---
+++ android/app/src/main/kotlin/com/example/tasksheet_input_validation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tasksheet_input_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
